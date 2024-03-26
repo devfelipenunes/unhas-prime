e5ea6431ebddbf1bc6d5a254a364182d7d623a42
devfelipenunes <devfelipenunes@gmail.com>
2024-03-26 11:03:12 -0300

    first

---
+++ src/app/Services/page.tsx
@@ -0,0 +1,154 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function Services() {
+  const [services, setServices] = useState([]);
+  const [totalExercises, setTotalExercises] = useState(0);
+  const [selectedName, setSelectedName] = useState("");
+  const router = useRouter();
+
+  useEffect(() => {
+    // Recuperar o nome selecionado armazenado em localStorage
+    const name = localStorage.getItem("selectedName");
+    setSelectedName(name);
+
+    // Recuperar os serviços salvos no localStorage de acordo com o nome selecionado
+    const savedData = JSON.parse(localStorage.getItem("savedData")) || [];
+    const selectedServices = savedData.find((data) => data.name === name);
+    if (selectedServices) {
+      setServices(selectedServices.services);
+    } else {
+      // Se não houver serviços salvos para o nome selecionado, inicialize com os serviços padrão
+      const serviceList = [
+        { servico: "Mão Simples", preco: 27.99, count: 0 },
+        { servico: "Mão Simples (Segunda a Quarta)", preco: 31.99, count: 0 },
+        { servico: "Cutilagem das mãos", preco: 24.99, count: 0 },
+        { servico: "Esmaltação das Mãos", preco: 21.99, count: 0 },
+        { servico: "Cortar e lixar", preco: 17.99, count: 0 },
+        { servico: "Unha decorada p/ dedo", preco: 5.0, count: 0 },
+        { servico: "Francesinha / adesivo", preco: 5.0, count: 0 },
+        { servico: "Pé Simples", preco: 29.99, count: 0 },
+        { servico: "Pé Simples (Segunda a Quarta)", preco: 34.99, count: 0 },
+        { servico: "Cutilagem dos pés", preco: 28.99, count: 0 },
+        { servico: "Esmaltação dos pés", preco: 21.99, count: 0 },
+        { servico: "Cortar/lixar(unha dos pés)", preco: 17.99, count: 0 },
+        { servico: "SPA dos pés Prime", preco: 59.99, count: 0 },
+        {
+          servico: "Mão e pé simples(Segunda a Quarta-feira)",
+          preco: 47.99,
+          count: 0,
+        },
+        {
+          servico: "Mão e pé simples (Quinta a sábado)",
+          preco: 51.99,
+          count: 0,
+        },
+        { servico: "Pé + mão + Spa Prime", preco: 89.99, count: 0 },
+        { servico: "Aplicação de Acrigel simples", preco: 164.99, count: 0 },
+        { servico: "Manutenção de Acrigel simples", preco: 109.99, count: 0 },
+        { servico: "Aplicação de fibra simples", preco: 179.99, count: 0 },
+        { servico: "Manutenção de fibra simples", preco: 139.99, count: 0 },
+        { servico: "Banho de gel simples", preco: 99.99, count: 0 },
+        { servico: "Manutenção Banho de gel", preco: 99.99, count: 0 },
+        { servico: "Postiça Simples", preco: 60.0, count: 0 },
+        { servico: "Postiça Realista", preco: 80.0, count: 0 },
+        { servico: "Esmaltação em gel", preco: 40.0, count: 0 },
+        { servico: "Francesinha com esmalte em gel", preco: 35.0, count: 0 },
+        { servico: "Babyboomer e Babyglitter(por dedo)", preco: 8.0, count: 0 },
+        { servico: "Pedrinha", preco: 3.0, count: 0 },
+        { servico: "Spaid gel", preco: 2.0, count: 0 },
+        { servico: "Encapsulada (por dedo)", preco: 8.0, count: 0 },
+        {
+          servico: "Remoção de Alongamento e Banho de gel",
+          preco: 29.99,
+          count: 0,
+        },
+        { servico: "Remoção de Esmaltação em gel", preco: 25.0, count: 0 },
+        { servico: "Troca de formato de Unha", preco: 15.0, count: 0 },
+        {
+          servico: "Manutenção de outra profissional tera um adicional",
+          preco: 20.0,
+          count: 0,
+        },
+        { servico: "Colocação (unidade)", preco: 10.0, count: 0 },
+      ];
+
+      setServices(serviceList);
+    }
+  }, [router]);
+
+  const handleSave = () => {
+    const savedData = JSON.parse(localStorage.getItem("savedData")) || [];
+    const newData = {
+      name: selectedName,
+      services: services,
+    };
+    const updatedData = [
+      ...savedData.filter((data) => data.name !== selectedName),
+      newData,
+    ];
+    localStorage.setItem("savedData", JSON.stringify(updatedData));
+    router.push("/Sumary");
+  };
+
+  const handleServiceChange = (index, action) => {
+    const updatedServices = [...services];
+    if (action === "increase") {
+      updatedServices[index].count++;
+      setTotalExercises(totalExercises + 1);
+    } else if (action === "decrease" && updatedServices[index].count > 0) {
+      updatedServices[index].count--;
+      setTotalExercises(totalExercises - 1);
+    }
+    setServices(updatedServices);
+
+    localStorage.setItem("services", JSON.stringify(updatedServices)); // Ajuste aqui
+  };
+
+  const renderServices = () => {
+    return services.map((service, index) => (
+      <div
+        className="w-full bg-blue-500 text-black flex flex-row justify-between px-3 h-14 rounded text-2xl"
+        key={index}
+      >
+        <div className="flex items-center">
+          <span className="font-bold text-black">{service.servico}</span>
+        </div>
+        <div className="flex flex-row space-x-2 items-center">
+          <button
+            className="h-full w-7 bg-blue-700"
+            onClick={() => handleServiceChange(index, "decrease")}
+          >
+            -
+          </button>
+          <span className="text-black font-bold">{service.count}</span>
+          <button
+            className="h-full w-7 bg-blue-700"
+            onClick={() => handleServiceChange(index, "increase")}
+          >
+            +
+          </button>
+        </div>
+      </div>
+    ));
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center py-11 text-black ">
+      <div className="flex  items-center justify-center space-y-4 my-12 w-full bg-slate-300 p-5">
+        <h2 className="text-3xl font-bold">{selectedName}'s Services</h2>
+      </div>
+      <div className="flex flex-col items-center justify-center space-y-4 my-12 w-full px-6">
+        {renderServices()}
+      </div>
+      <button
+        className=" bg-slate-300 flex flex-row justify-between p-5 rounded text-black"
+        onClick={handleSave}
+      >
+        Salvar
+      </button>
+    </div>
+  );
+}
---
+++ src/app/Sumary/page.tsx
@@ -0,0 +1,96 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function Summary() {
+  const [storedData, setStoredData] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(null); // Alteração: Inicializar currentIndex como null
+  const router = useRouter();
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const savedData = JSON.parse(localStorage.getItem("savedData"));
+      if (savedData) {
+        setStoredData(savedData);
+      }
+    }
+  }, []);
+
+  // Função para calcular o valor total dos serviços
+  const calculateTotal = (services) => {
+    return services.reduce((total, service) => {
+      return total + service.count * service.preco;
+    }, 0);
+  };
+
+  // Função para calcular o valor que a manicure receberá (35% do valor total)
+  const calculateManicureShare = (total) => {
+    const manicureShare = total * 0.35;
+    return manicureShare.toFixed(2).replace(".", ",");
+  };
+
+  // Função para formatar o valor com vírgula e duas casas decimais
+  const formatValue = (value) => {
+    return value.toFixed(2).replace(".", ",");
+  };
+
+  const goBack = () => {
+    router.push("/");
+  };
+
+  const handleNameClick = (index) => {
+    setCurrentIndex(index === currentIndex ? null : index); // Alteração: Alternar currentIndex entre o índice atual e null se já estiver selecionado
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center w-full p-4 text-black">
+      <h1>Summary</h1>
+      <div className="my-4 w-full">
+        {storedData.map((data, index) => {
+          const total = calculateTotal(data.services);
+          const formattedTotal = formatValue(total);
+          const manicureShare = calculateManicureShare(total);
+          return (
+            <div
+              className="w-full my-5"
+              key={index}
+            >
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex flex-row justify-between w-full"
+                onClick={() => handleNameClick(index)}
+              >
+                <h2>{data.name}</h2>
+                <div>{currentIndex === index ? "▲" : "▼"}</div>
+              </button>
+              {currentIndex === index && ( // Alteração: Renderizar o conteúdo apenas se currentIndex corresponder ao índice atual
+                <div className="flex flex-row justify-between w-full bg-slate-300 p-5">
+                  <ul>
+                    {data.services
+                      .filter((service) => service.count > 0)
+                      .map((service, idx) => (
+                        <li key={idx}>
+                          ({service.count}) {service.servico}:{" "}
+                          {formatValue(service.count * service.preco)}
+                        </li>
+                      ))}
+                  </ul>
+                  <div>
+                    <p className="text-right">Total: {formattedTotal}</p>
+                    <p>Manicure receberá: {manicureShare}</p>
+                  </div>
+                </div>
+              )}
+            </div>
+          );
+        })}
+      </div>
+
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => goBack()}
+      >
+        Home
+      </button>
+    </div>
+  );
+}
